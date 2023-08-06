2e2e9334084bf4bebbd75e365af85634646c1c2a
Bohdan99I <bohdan.voiyov@gmail.com>
2023-08-06 18:50:11 +0300

    Hooks/App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Notiflix from 'notiflix';
 import { getImages } from '../services/pixabayAPI';
 import { Searchbar } from './Searchbar/Searchbar';
@@ -8,106 +8,83 @@ import { Loader } from './Loader/Loader';
 import { Modal } from './Modal/Modal';
 import { Container } from './App.styled';
 
-export class App extends Component {
-  state = {
-    images: [],
-    page: 1,
-    value: '',
-    totalPages: 0,
-    isLoading: false,
-    showModal: false,
-    largeImageURL: '',
-  };
+export const App = () => {
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [value, setValue] = useState('');
+  const [totalPages, setTotalPages] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [largeImageURL, setLargeImageURL] = useState('');
 
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      this.state.page !== prevState.page ||
-      this.state.value !== prevState.value
-    ) {
-      this.getImagesToArray(prevState);
+  useEffect(() => {
+    if (page !== 1 || value !== '') {
+      getImagesToArray();
     }
-  }
+  }, [page, value]);
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
 
-  openModal = id => {
-    this.setState({ isLoading: true });
-    const largeImage = this.state.images.find(image => image.id === id);
+  const openModal = id => {
+    setIsLoading(true);
+    const largeImage = images.find(image => image.id === id);
 
     setTimeout(() => {
-      this.setState({
-        largeImageURL: largeImage.largeImageURL,
-        isLoading: false,
-      });
-      this.toggleModal();
+      setLargeImageURL(largeImage.largeImageURL);
+      setIsLoading(false);
+      toggleModal();
     }, 500);
   };
 
-  handleSubmit = value => {
-    this.setState({ images: [], value: value, page: 1 });
+  const handleSubmit = value => {
+    setImages([]);
+    setValue(value);
+    setPage(1);
     if (!value) {
       Notiflix.Notify.warning('Enter your request!');
     }
   };
 
-  buttonOnClick = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const buttonOnClick = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  getImagesToArray = async prevState => {
+  const getImagesToArray = async () => {
     try {
-      this.setState({ isLoading: true });
-      const { value, page } = this.state;
+      setIsLoading(true);
       const { totalHits, hits } = await getImages(value, page);
       const pageCount = totalHits / 12;
-      this.setState({
-        totalPages: pageCount,
-      });
+      setTotalPages(pageCount);
       setTimeout(() => {
-        if (page !== prevState.page) {
-          this.setState(prevState => ({
-            images: [...prevState.images, ...hits],
-            isLoading: false,
-          }));
+        if (page !== 1) {
+          setImages(prevImages => [...prevImages, ...hits]);
         } else {
-          this.setState({ images: hits, isLoading: false });
+          setImages(hits);
         }
+        setIsLoading(false);
       }, 1000);
     } catch (error) {
       console.log(error);
     }
   };
 
-  render() {
-    const {
-      images,
-      value,
-      isLoading,
-      page,
-      totalPages,
-      showModal,
-      largeImageURL,
-    } = this.state;
-    return (
-      <Container>
-        {isLoading && <Loader />}
-        <Searchbar onSubmit={this.handleSubmit} />
-        {value !== '' && (
-          <>
-            <ImageGallery images={images} openModal={this.openModal} />
-            {page < totalPages && <Button onClick={this.buttonOnClick} />}
-          </>
-        )}
-        {showModal && (
-          <Modal onClose={this.toggleModal}>
-            <img src={largeImageURL} alt={value} />
-          </Modal>
-        )}
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      {isLoading && <Loader />}
+      <Searchbar onSubmit={handleSubmit} />
+      {value !== '' && (
+        <>
+          <ImageGallery images={images} openModal={openModal} />
+          {page < totalPages && <Button onClick={buttonOnClick} />}
+        </>
+      )}
+      {showModal && (
+        <Modal onClose={toggleModal}>
+          <img src={largeImageURL} alt={value} />
+        </Modal>
+      )}
+    </Container>
+  );
+};
